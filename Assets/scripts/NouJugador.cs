8c0160ce42da917fe206d2095d17d7f5167c5364
David Pereda Rentero <davidpereda@ceroca.cat>
2025-09-25 17:56:52 +0200

    Control limits pantall iniciat

--- Assets/scripts/NouJugador.cs
+++ Assets/scripts/NouJugador.cs
@@ -1,32 +1,55 @@
+using System;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float vel = 10f;
+    private Camera camera;
     //public GameObject jugador;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        MovimentNau();
+    }
+    void MovimentNau()
+    {
         /*if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
             
         }*/
+        //Control limits pantalla
+
+        Vector3 limitInferiorEsquerra = camera.ViewportToWorldPoint(new Vector2 (0, 0));
+        Vector3 limitSuperiorDret = camera.ViewportToWorldPoint(new Vector2(1, 1));
+        //Moviment nau.
         float direccioHoritzontal = Input.GetAxisRaw("Horizontal");
         float direccioVertical = Input.GetAxisRaw("Vertical");
         Vector3 direccio = new Vector3(direccioHoritzontal, direccioVertical, 0).normalized;// (x, y, z)
         Debug.Log("direccio =" + direccio);
         //transform.position = direccio;
 
-        Vector3 nouDesplacament = new Vector3(vel * direccio.x * Time.deltaTime,
-        vel * direccio.y * Time.deltaTime, vel * direccio.z * Time.deltaTime);
+        Vector3 nouDesplacament = new Vector3(
+        vel * direccio.x * Time.deltaTime,
+        vel * direccio.y * Time.deltaTime,
+        vel * direccio.z * Time.deltaTime);
         //Debug.Log("Time.deltaTime =" + Time.deltaTime);
+
+        //Control dels limits de la pantalla
+        //El metode Math.Clamp, ens retoran el primer parametre
+        // Pero, si aquest primer parametre, te un valor mes petit que el segon parametre
+        //  retornara el segon parametre,. I, si te un valor mes gran que el tercer
+        //   parametre, ens retornara el tercer parametre
+        nouDesplacament.x = Math.Clamp(nouDesplacament.x, limitInferiorEsquerra.x, limitSuperiorDret.x);
+        nouDesplacament.y = Math.Clamp(nouDesplacament.y, limitInferiorEsquerra.y, limitSuperiorDret.y);
+
         //Apliquem el vector desplaçament a l'objecte
-        transform.position += nouDesplacament; 
+        transform.position += nouDesplacament;
     }
 }
